87d6f9dbf984c596f415c2f0e008594157696454
GitHub Action <action@github.com>
2023-03-14 19:28:02 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "ai_code_reviewer"
--- src/main/kotlin/com/github/omerahmed41/aicodereviewer/MyBundle.kt
+++ src/main/kotlin/com/github/omerahmed41/aicodereviewer/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.omerahmed41.aicodereviewer
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
